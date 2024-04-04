8b289bbd7550ecaa8caa4f1bc5aea2420ba1e112
pelletierr <drizzt@menzonet.org>
2024-04-04 16:00:48 -0400

    Update ADenum.py

--- ADenum.py
+++ ADenum.py
@@ -90,7 +90,7 @@ class LdapEnum:
     
                 while True:
                     # Perform LDAP search with paging control
-                    msgid = self.ldapCon.search_ext_s(self.baseDn, ldap.SCOPE_SUBTREE, OBJECT_TO_SEARCH, ATTRIBUTES_TO_SEARCH, 0, serverctrls=server_controls, clientctrls='none', -1, 100)
+                    msgid = self.ldapCon.search_ext_s(self.baseDn, ldap.SCOPE_SUBTREE, OBJECT_TO_SEARCH, ATTRIBUTES_TO_SEARCH, 0, serverctrls=server_controls, clientctrls='none')
                     _, result_data, _, serverctrls = self.ldapCon.result3(msgid)
     
                     # Add the search results to resultSearch list
